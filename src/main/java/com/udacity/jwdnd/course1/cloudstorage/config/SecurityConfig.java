3c648308f47101db5f27b447616f1915f4b816ab
ahn-nath <67247850+ahn-nath@users.noreply.github.com>
2021-02-17 16:32:07 -0400

    fix: handle max upload size exception

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
 
@@ -47,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.permitAll();
         
     }
+    
 
 
 }
